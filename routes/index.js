3e762805ced3febaeb3562b6620cb42edf6d5367
Andreas Kuensken <andreas@kuensken-online.de>
2014-09-13 16:23:50 +0200

    Changed Title Header for main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Tippspiel' });
+  res.render('index', { title: 'Tippspiel Hauptseite' });
 });
 
 module.exports = router;
