8940b352b9eeb8ca097a87468f7af4fea14737fe
Andreas Kuensken <andreas@kuensken-online.de>
2014-09-26 21:32:17 +0200

    Adapted Title and Heading for results page

--- routes/ergebnisse.js
+++ routes/ergebnisse.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('ergebnisse', { title: 'Bitte Spieltag auswaehlen und Ergebnisse eintragen.' });
+  res.render('ergebnisse');
 });
 
 module.exports = router;
